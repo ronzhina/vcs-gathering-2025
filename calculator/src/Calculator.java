b0a40a36abf447027128f0ac396585922c9ecfd3
Anastasia.Ronzhina <anastasia.ronzhina@jetbrains.com>
2025-01-29 12:46:12 +0100

    another fix

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
     }
 
     public int sub(int firstNumber, int secondNumber) {
-        return firstNumber - secondNumber;
+        return firstNumber / secondNumber;
     }
 
     public int mod(int a, int b) {
